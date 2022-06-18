79c375b43b74feb4b9ac420af2756d59a8197d81
NabizaBatool <nabizarizvi1@gmail.com>
2022-06-18 21:34:19 +0500

    sol

--- module5-sol/restaurant/js/ajax-utils.js
+++ module5-sol/restaurant/js/ajax-utils.js
@@ -1,3 +1,6 @@
+
+
+
 (function (global) {
 
 // Set up a namespace for our utility
--- module5-sol/restaurant/js/script.js
+++ module5-sol/restaurant/js/script.js
@@ -18,6 +18,11 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
     var dc = {};
     var homeHtml = "snippets/home-snippet.html";
+    var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
+    var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+    var categoryHtml = "snippets/category-snippet.html";
+
+
   
 
     // Convenience function for inserting innerHTML for 'select'
@@ -36,8 +41,15 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
     // Return substitute of '{{propName}}'
     // with propValue in given 'string'
+    // g means replace it everywhere not the first tym u see it , {{ propName }} shows namespaces , string reprsent the entire code in snippet
 
-    
+    var insertProperty = function (string, propName, propValue) {
+        var propToReplace = "{{" + propName + "}}";
+        //flag g means replace eveywhere 
+        string = string.replace(new RegExp(propToReplace, "g"), propValue);
+        return string;
+    };
+
 
 
     // On page load (before images or CSS)
@@ -51,12 +63,65 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 document.querySelector("#main-content").innerHTML = responseText;
             },
             false);
-    });
+        });
+
+    // Load the menu categories view
+    dc.loadMenuCategories = function () {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            allCategoriesUrl,
+            buildAndShowCategoriesHTML);
+    };
+
+ // Builds HTML for the categories page based on the data
+ // from the server
+ //categories contain all categories , it is passed as parameter 
+    function buildAndShowCategoriesHTML(categories) {
+        // Load title snippet of categories page
+        $ajaxUtils.sendGetRequest(
+            categoriesTitleHtml,
+            function (categoriesTitleHtml) {
+                // Retrieve single category snippet
+                $ajaxUtils.sendGetRequest(
+                    categoryHtml,
+                    function (categoryHtml) {
+                        var categoriesViewHtml =
+                            buildCategoriesViewHtml(categories,
+                                categoriesTitleHtml,
+                                categoryHtml);
+                        insertHtml("#main-content", categoriesViewHtml);
+                    },
+                    false);
+            },
+            false);
+    }
 
 
-   
 
+    function buildCategoriesViewHtml(categories,
+        categoriesTitleHtml,
+        categoryHtml) {
 
+        var finalHtml = categoriesTitleHtml;
+        finalHtml += "<section class='row'>";
+
+        // Loop over categories
+        for (var i = 0; i < categories.length; i++) {
+            // Insert category values
+            var html = categoryHtml;
+            var name = "" + categories[i].name;
+            var short_name = categories[i].short_name;
+            html = insertProperty(html, "name", name);
+            html =
+                insertProperty(html,
+                    "short_name",
+                    short_name);
+            finalHtml += html;
+        }
+
+        finalHtml += "</section>";
+        return finalHtml;
+    }
 
 
     // $dc name with which we expose it to other script
